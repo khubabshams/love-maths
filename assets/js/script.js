709de8bed740ceff195ac3a67a134a229bccd61e
Khubab <khubab220@gmail.com>
2022-09-14 10:47:04 +0000

    Add events for dom loading and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons  = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") == 'submit'){
+                
+            }
+        });
+    }
+});
 function runGame(){
 
 }
